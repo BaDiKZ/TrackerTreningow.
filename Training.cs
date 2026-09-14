ed4d198850fd66ceec4309c8ed9f0c719fafd57d
Praktykant 2 <praktykant2@psoft.local>
2026-09-14 15:48:51 +0200

    added new property to traning - isImportant

--- Training.cs
+++ Training.cs
@@ -2,15 +2,11 @@
 {
     public class Training
     {
-        // Niepowtarzalny identyfikator — przyda się przy edycji i usuwaniu.
         public Guid Id { get; set; } = Guid.NewGuid();
-
         public DateTime Date { get; set; } = DateTime.Today;
-
         public string Type { get; set; } = "Inne";
-
         public int Minutes { get; set; }
-
         public string Note { get; set; } = "";
+        public bool IsImportant { get; set; }
     }
 }
